09ccb40248b6ff897f440d88e5f5cda8cef5633e
Govind-gothi <govindgothi3@gmail.com>
2026-07-13 20:30:46 +0530

    Fix DB_CONFIG

--- app.py
+++ app.py
@@ -9,6 +9,17 @@ import os
 app = Flask(__name__)
 app.secret_key = 'service-provider-marketplace-secret-key'
 
+app = Flask(__name__)
+app.secret_key = 'service-provider-marketplace-secret-key'
+
+DB_CONFIG = {
+    "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT")),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME")
+}
+
 def get_db():
     print("DB_CONFIG =", DB_CONFIG)
     print("DB_HOST =", os.getenv("DB_HOST"))
